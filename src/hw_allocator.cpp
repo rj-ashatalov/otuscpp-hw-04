4e4d818895f1d011622713b218bed69928b0ba3d
Shatalov Andrey <a.shatalov@rj-games.com>
2019-11-04 23:08:54 +0300

    wip

--- src/hw_allocator.cpp
+++ src/hw_allocator.cpp
@@ -77,21 +77,33 @@ struct HwAllocator
 
     HwAllocator() = default;
 
-    template <class U, size_t M>
-    constexpr HwAllocator(const HwAllocator<U, M>&) noexcept {}
+    template <class U>
+    constexpr HwAllocator(const HwAllocator<U, N>&) noexcept {}
 
     [[nodiscard]] T* allocate(std::size_t n)
     {
         std::cout << __PRETTY_FUNCTION__ << std::endl;
-        if(n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+
+        if (n > _allocRemain)
         {
-            throw std::bad_alloc();
+            auto p = reserve(N + _size);
+            for (auto i = 0u; i < (_size - _allocRemain); ++i)
+            {
+//                std::swap(*(p+i), *(_mempool+i));
+//                new(p+i) T(std::forward<T>(*(_mempool + i)));
+                auto&& tmp = *(_mempool + i);
+                *(p + i) = std::move(*(_mempool + i));
+            }
+            _lastPointer = p + _size - _allocRemain - 1;
+            _allocRemain = N;
+            _mempool = p;
+            _size += N;
         }
-        if(auto p = static_cast<T*>(std::malloc(n*sizeof(T))))
-        {
-            return p;
-        }
-        throw std::bad_alloc();
+
+        _allocRemain -= n;
+        auto currentPointer = _lastPointer;
+        _lastPointer += n;
+        return currentPointer;
     }
 
     void deallocate(T* p, std::size_t) noexcept
@@ -99,6 +111,27 @@ struct HwAllocator
         std::cout << __PRETTY_FUNCTION__ << std::endl;
         std::free(p);
     }
+
+    private:
+        T* reserve(std::size_t n)
+        {
+            std::cout << __PRETTY_FUNCTION__ << std::endl;
+            if(n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+            {
+                throw std::bad_alloc();
+            }
+            if(auto p = static_cast<T*>(std::malloc(n*sizeof(T))))
+            {
+                return p;
+            }
+            throw std::bad_alloc();
+        }
+
+
+        size_t _size = 0u;
+        size_t _allocRemain = 0u;
+        T * _mempool = nullptr;
+        T * _lastPointer = nullptr;
 };
 template <class T, size_t N, class U, size_t M>
 bool operator==(const HwAllocator<T, N>&, const HwAllocator<U, M>&) { return (N == M) && std::is_same_v<T,U>; }
